6768c26128b6e98c17370055186a1be663581a47
MercMayhem <amanrao032@gmail.com>
2024-02-09 18:28:48 +0530

    learnin TCP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(stream: TcpStream) {
+    match stream.peer_addr(){
+        Ok(addr) => println!("connection with address: {:?}", addr),
+        Err(e) => println!("Error: {:?}", e)
+    }
 }
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(e) => println!("Error: {:?}", e)
+        }
+    }
+    Ok(())
+}
+

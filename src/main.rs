6c2511030e5b4f1e00fe2657d798030a132c8271
MercMayhem <amanrao032@gmail.com>
2024-02-13 12:41:06 +0530

    formatted correctly

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::Read;
-use std::time::Duration;
+mod request;
 use httparse::{parse_headers, EMPTY_HEADER};
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 fn handle_connection(mut stream: TcpStream) {
-    match stream.peer_addr(){
+    match stream.peer_addr() {
         Ok(addr) => println!("connection with address: {:?}", addr),
-        Err(e) => println!("Error: {:?}", e)
+        Err(e) => println!("Error: {:?}", e),
     };
-        let _ = stream.set_read_timeout(Some(Duration::new(5, 0)));
-        let mut buf: [u8; 4096] = [0; 4096];
+    let _ = stream.set_read_timeout(Some(Duration::new(5, 0)));
+    let mut buf: [u8; 4096] = [0; 4096];
 
-        let bytes:usize = stream.read(&mut buf).unwrap();
+    let bytes: usize = stream.read(&mut buf).unwrap();
 
-        let message: String = String::from_utf8(buf[..bytes].to_vec()).unwrap();
-        let header_start: usize = message.find('\n').unwrap() + 1;
-        let mut headers = [EMPTY_HEADER; 20];
+    let message: String = String::from_utf8(buf[..bytes].to_vec()).unwrap();
+    let header_start: usize = message.find('\n').unwrap() + 1;
+    let mut headers = [EMPTY_HEADER; 20];
 
-        let parsed_headers = parse_headers(message[header_start..].as_bytes(), &mut headers).unwrap();
+    let parsed_headers = parse_headers(message[header_start..].as_bytes(), &mut headers).unwrap();
 
-        println!("data: {}", message);
-        println!("header start: {:?}", header_start);
-        println!("header: \n{}", message[header_start ..].to_string());
-        println!("{:?}", parsed_headers);
+    println!("data: {}", message);
+    println!("header start: {:?}", header_start);
+    println!("header: \n{}", message[header_start..].to_string());
+    println!("{:?}", parsed_headers);
 }
 
 fn main() -> std::io::Result<()> {
@@ -33,9 +34,8 @@ fn main() -> std::io::Result<()> {
             Ok(stream) => {
                 handle_connection(stream);
             }
-            Err(e) => println!("Error: {:?}", e)
+            Err(e) => println!("Error: {:?}", e),
         }
     }
     Ok(())
 }
-

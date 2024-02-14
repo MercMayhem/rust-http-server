897e6382e1891c4edc3e2858cb826b36593ec905
MercMayhem <amanrao032@gmail.com>
2024-02-14 17:24:18 +0530

    integrated HttpRequest into main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod request;
-use httparse::{parse_headers, EMPTY_HEADER};
+use httparse::EMPTY_HEADER;
+use request::HttpRequest;
 use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
@@ -12,22 +13,14 @@ fn handle_connection(mut stream: TcpStream) {
     let _ = stream.set_read_timeout(Some(Duration::new(5, 0)));
     let mut buf: [u8; 4096] = [0; 4096];
 
-    let bytes: usize = stream.read(&mut buf).unwrap();
+    let _bytes: usize = stream.read(&mut buf).unwrap();
+    let mut header_arr = [EMPTY_HEADER; 64];
 
-    let message: String = String::from_utf8(buf[..bytes].to_vec()).unwrap();
-    let request_type_end: usize = message
-        .find(' ')
-        .unwrap();
-    let header_start: usize = message.find('\n').unwrap() + 1;
-    let mut headers = [EMPTY_HEADER; 20];
-
-    let parsed_headers = parse_headers(message[header_start..].as_bytes(), &mut headers).unwrap();
-
-    println!("data: {}", message);
-    println!("request type: {}", message[..request_type_end].to_string());
-    println!("header start: {:?}", header_start);
-    println!("header: \n{}", message[header_start..].to_string());
-    println!("{:?}", parsed_headers);
+    let request = HttpRequest::new(&buf, header_arr.as_mut_slice()).unwrap();
+    
+    println!("Type: {:?}", request.request_type);
+    println!("Header: {:?}", request.headers);
+    println!("Resource: {:?}", request.resource)
 }
 
 fn main() -> std::io::Result<()> {

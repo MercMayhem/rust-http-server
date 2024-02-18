e95367187e4fa2e410e8bcca04c97d501a069e5c
MercMayhem <amanrao032@gmail.com>
2024-02-18 19:58:29 +0530

    created HttpResponse struct

---
+++ src/response.rs
@@ -0,0 +1,63 @@
+use std::collections::HashMap;
+
+use chrono::Local;
+
+pub struct HttpResponse<'a> {
+    status: u8,
+    headers: HashMap<String, String>,
+    body: &'a str,
+}
+
+impl<'a> HttpResponse<'a> {
+    pub fn from_html_body(html: &'a str) -> HttpResponse<'a> {
+        let status = 200;
+        let mut headers = HashMap::new();
+
+        headers.insert(
+            "Content-Length".to_string(),
+            format!("{}", html.bytes().len()),
+        );
+        headers.insert(
+            "Content-type".to_string(),
+            "text/html; charset=utf-8".to_string(),
+        );
+        headers.insert("Server".to_string(), "Amans-Rust-Server/0.1".to_string());
+        headers.insert("Date".to_string(), Local::now().to_rfc2822().to_string());
+
+        let body = html;
+
+        return HttpResponse {
+            status,
+            headers,
+            body,
+        };
+    }
+
+    fn push_headers(&self, ret: &mut String) {
+        for (key, value) in self.headers.iter() {
+            ret.push_str(format!("{}: {}\r\n", key.as_str(), value.as_str()).as_str());
+        }
+        ret.push_str("\r\n")
+    }
+}
+
+impl<'a> ToString for HttpResponse<'a> {
+    fn to_string(&self) -> String {
+        let mut ret = String::new();
+
+        ret.push_str("HTTP/1.1 ");
+        ret.push_str(format!("{} ", self.status).as_str());
+
+        match self.status {
+            200 => ret.push_str("OK"),
+            _ => println!("We'll cross this bridge when we get to it"),
+        }
+
+        ret.push_str("\r\n");
+
+        self.push_headers(&mut ret);
+        ret.push_str(self.body);
+
+        return ret;
+    }
+}
